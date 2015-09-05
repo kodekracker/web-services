82669aa297d7b9f488ada03d6ee37718470ebe46
Akshay Pratap Singh <pratapakshay0@gmail.com>
2015-09-05 23:31:34 +0530

    (feat): added rss feeds support for blog api
    * updated .gitignore and deleted staticfiles folder
    * fixed api root response

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken.views import obtain_auth_token
-from app.views import api_root, send_mail, get_tweets
+from app.views import api_root, send_mail, get_tweets, get_blogs
 
 urlpatterns = [
     url(r'^api/auth-token/$', obtain_auth_token, name="get-auth-token"),
     url(r'^api/mails/$', send_mail, name='send-mail'),
     url(r'^api/tweets/$', get_tweets, name='tweet-list'),
+    url(r'^api/blogs/$', get_blogs, name='blog-list'),
     url(r'^$', api_root),
 ]
 
